9b5272af305483ebe52c04565b14b270ffa3f5ed
eswaribala <parameswaribala@gmail.com>
2021-05-26 15:06:11 +0530

    modified

--- apigateway/src/main/java/com/ericsson/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/ericsson/apigateway/ApigatewayApplication.java
@@ -3,6 +3,8 @@ package com.ericsson.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,6 +12,10 @@ public class ApigatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
